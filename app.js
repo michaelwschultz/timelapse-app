5af187678d553b0d753b0936e051bb8015e349e9
Michael Schultz <michaelwschultz@gmail.com>
2017-10-20 12:59:38 -0700

    Adds check for getUserMedia and webki or moz detection

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-navigator.webkitGetUserMedia({video: true},
-  function(stream) {
-    var video = document.getElementById('camera');
-    video.srcObject = stream;
+navigator.getUserMedia = navigator.getUserMedia ||
+                         navigator.webkitGetUserMedia ||
+                         navigator.mozGetUserMedia;
 
-    video.onloadedmetadata = function(e) {
-      video.classList.add('fadeInOnce');
-      console.log('Video feed live!')
-   };
-  },
-  function() {
-    alert('Could not connect to camera!');
-  }
-);
+if (navigator.getUserMedia) {
+    navigator.getUserMedia({video: true},
+        function(stream) {
+            var video = document.getElementById('camera');
+            video.srcObject = stream;
+
+            video.onloadedmetadata = function(e) {
+                video.play();
+                video.classList.add('fadeInOnce');
+                console.log('Video feed live!')
+            };
+        },
+        function() {
+            alert('Could not connect to camera!');
+        }
+    );
+} else {
+   console.log("getUserMedia not supported.");
+}
