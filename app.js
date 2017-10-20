46640b04bbc4c16c298bc20b04ba9ee8c12a1750
Michael Schultz <michaelwschultz@gmail.com>
2017-10-20 13:32:35 -0700

    Updates depreciated getUserMedia method.

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
-navigator.getUserMedia = navigator.getUserMedia ||
-                         navigator.webkitGetUserMedia ||
-                         navigator.mozGetUserMedia;
+if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({video: true})
+    .then(function(stream) {
+        console.log("- warming up camera")
 
-if (navigator.getUserMedia) {
-    navigator.getUserMedia({video: true},
-        function(stream) {
-            var status = document.getElementById("status");
-            var video = document.getElementById('camera');
-            video.srcObject = stream;
-            status.innerHTML = "Warming up camera...";
+        var status = document.getElementById("status");
+        var video = document.getElementById('camera');
+        video.srcObject = stream;
+        status.innerHTML = "Warming up camera...";
 
-            video.onloadedmetadata = function(e) {
-                video.play();
-                video.classList.add('fadeInOnce');
-                console.log('Video feed live!');
+        video.onloadedmetadata = function(e) {
+            video.play();
+            console.log('* video feed live');
 
-                // removes status element
-                status.parentNode.removeChild(status);
-            };
-        },
-        function() {
-            status.innerHTML = "Could not connect to camera!";
-        }
-    );
+            video.classList.add('fadeInOnce');
+            
+            // removes status element
+            status.parentNode.removeChild(status);
+        };
+    })
+    .catch(function() {
+        status.innerHTML = "Could not connect to camera!";
+    });
 } else {
     document.getElementById("status").innerHTML = "Sorry, can't find your webcam.";
     console.log("getUserMedia not supported.");
